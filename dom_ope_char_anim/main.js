5779b5faa54a33d56f0a9f57b19d039be320591b
ko1nkgw <ko1nkgw@gmail.com>
2020-06-14 11:28:28 +0900

    アニメーション処理のクラス化

--- dom_ope_char_anim/main.js
+++ dom_ope_char_anim/main.js
@@ -1,12 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const el = document.querySelector(".animate-title");
-    const str = el.innerHTML.trim();
-
-    el.innerHTML = str.split('').reduce((accu, curr) => {
-        curr = curr.replace(" ", "&nbsp;");
-        accu += `<span class="char">${curr}</span>`;
-        return accu;
-        // console.log(accu);
-    }, "");
-
+    const ta = new TextAnimation(".animate-title");
+    const ta2 = new TextAnimation(".animate-title-2");
+    setTimeout(() => {
+        ta.animate();
+        ta2.animate();
+    }, 1000);
 });
+class TextAnimation {
+    constructor(el) {
+        this.el = document.querySelector(el);
+        this.chars = this.el.innerHTML.trim().split("");
+        this.el.innerHTML = this._spanText();
+        console.log(this.chars);
+    }
+    _spanText() {
+        return this.chars.reduce((accu, curr) => {
+            curr = curr.replace(" ", "&nbsp;");
+            accu += `<span class="char">${curr}</span>`;
+            return accu;
+        }, "");        
+    }
+    animate() {
+        this.el.classList.toggle('inview');
+    }
+}
